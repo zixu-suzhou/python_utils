fe821949f7a8c6353eaea98a5c84a27d5b36879f
张冬华 <zhangdonghua@zhangdonghuadeMacBook-Pro.local>
2022-04-11 23:36:46 +0800

    modify long run script

--- reboot_test.sh
+++ reboot_test.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-i=2
+i=10000
+run=0
 
 while [ $i -ge 1 ]
 do
+  echo 'running' $run 'times'
   cd /opt/updater/denny/deploy/modules/camera_pipeline/scripts/
   sh run_camera.sh &
-  sleep 180
+  sleep 60
   slay -s2 mfrlaunch
-  sleep 120
+  sleep 50
   ((i--))
+  ((run++))
 done
