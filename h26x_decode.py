88d3f2073ee1957692700b8f9148863198537ed7
ZHANG Denny (XC-AS/ENG-PED3-CN) <denny.zhang@cn.bosch.com>
2025-10-08 10:04:05 +0800

    chore: remove unnessary files

--- h26x_decode.py
+++
@@ -1,9 +0,0 @@
-import imageio
-
-input_file = 'front_wide.h264'
-output_prefix = 'output'
-
-reader = imageio.get_reader(input_file)
-for i, frame in enumerate(reader):
-    imageio.imwrite(f'{output_prefix}_{i:04d}.png', frame)
-
